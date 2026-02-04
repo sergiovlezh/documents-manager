f31eee60b99bde54c1b3920b8738d3d14dd6ebb3
Sergio Velez <sergio.vlezh@gmail.com>
2026-02-04 02:18:46 +0000

    fix: Update Model ViewSet `create` method to use a different serializer in the request and the response.

--- documents/views.py
+++ documents/views.py
@@ -1,5 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from documents.models import Document
 from documents.serializers import (
@@ -55,3 +56,13 @@ class DocumentViewSet(viewsets.ModelViewSet):
             return DocumentDetailSerializer
 
         return DocumentListSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        document = serializer.save()
+
+        response_serializer = DocumentDetailSerializer(
+            document, context=self.get_serializer_context()
+        )
+        return Response(response_serializer.data, status=status.HTTP_201_CREATED)
