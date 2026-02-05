192d0a8c3f3ed3a0f9645d965102a9ffb7affa26
Sergio Velez <sergio.vlezh@gmail.com>
2026-02-05 05:43:40 +0000

    Configure Django REST Framework and update API URLs.
    - Set default authentication and permissions in `REST_FRAMEWORK`.
    - Prefix `documents` URLs with `api/` and add `api-auth/` for browsable API login.

--- config/settings/base.py
+++ config/settings/base.py
@@ -93,3 +93,14 @@ STATIC_URL = "static/"
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = BASE_DIR / "media"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
