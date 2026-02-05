bb4f211f7126edeabf4268111f13216d71a7a077
Sergio Velez <sergio.vlezh@gmail.com>
2026-02-05 05:52:26 +0000

    fix: Add missing installed app.

--- config/settings/base.py
+++ config/settings/base.py
@@ -18,7 +18,9 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
-THIRD_PARTY_APPS: list[str] = []
+THIRD_PARTY_APPS: list[str] = [
+    "rest_framework",
+]
 
 LOCAL_APPS: list[str] = [
     "documents",
