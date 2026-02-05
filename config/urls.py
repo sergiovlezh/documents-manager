d6bd2318f245509ed02ff40b5af8c579f6ec2cc1
Sergio Velez <sergio.vlezh@gmail.com>
2026-02-05 06:10:58 +0000

    fix: Serve media files in debug mode.

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
+
+from django.conf import settings
 
 urlpatterns = [
     # Admin
@@ -21,3 +24,9 @@ urlpatterns = [
         name="swagger-ui",
     ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
